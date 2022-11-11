44a511cd348141b868ae025f7e1b9241cd53e27f
Angel <10147562+AngelSlava@users.noreply.github.com>
2022-11-11 17:13:00 +0200

    adding dist

---
+++ js/about.7e71fa9e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcrud_table_qfs"]=self["webpackChunkcrud_table_qfs"]||[]).push([[443],{2323:function(t,u,n){n.r(u),n.d(u,{default:function(){return c}});var s=function(){var t=this;t._self._c;return t._m(0)},e=[function(){var t=this,u=t._self._c;return u("div",{staticClass:"about"},[u("h1",[t._v("This is an about page")])])}],a=n(1001),l={},r=(0,a.Z)(l,s,e,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about.7e71fa9e.js.map
